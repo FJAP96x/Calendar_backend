79a61a8b07c2df34814da3c8547270891d46706f
FJAP96x <lx02918@innovacion.mx>
2025-06-04 18:21:29 -0600

    backend 1.0.1 finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express")
 require("dotenv").config()
 const cors = require("cors")
@@ -23,6 +24,11 @@ app.use(express.json())
 //TODO: auth // create,login, renewToken
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/events", require("./routes/events"))
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
 //TODO: CRUD: Events
 
 
